edb2f5edb034e317dd52ee7f4184c243fc93c54e
David McCaslin <davidmccaslin94@gmail.com>
2020-08-28 12:44:37 -0700

    update business logic and test specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,42 @@
 // Business Logic for Pizza
 function Pizza(Size,toppings) {
   this.Size = Size,
-  this.topping = toppings
+  this.toppings = toppings
 }
 
 Pizza.prototype.cost = function() {
-  //let baseCost;
-  /*
-  const size = this.size;
-  if (size == "small") {
-    baseCost = 10;
-  } else if (size == "medium") {
-    baseCost = 15;
-  } else if (size == "large") {
-    baseCost = 20;
-  }
-  */
   const baseCost = this.Size.baseCost;
   console.log(baseCost);
   const toppingCost = this.toppings.length * 2;
   console.log(toppingCost);
   this.cost = baseCost + toppingCost;
+  console.log(this.cost);
 }
 
 // Business Logic for Size
 function Size(name,baseCost) {
   this.name = name,
-  this.baseCost = baseCost,
+  this.baseCost = baseCost
 }
 
 const smallSize = new Size("small",10);
 const mediumSize = new Size("medium",15);
 const largeSize = new Size("large",20);
+
+let sizes = [];
+sizes.push(smallSize);
+sizes.push(mediumSize);
+sizes.push(largeSize);
+
+// Business Logic for Topping
+
+let toppings = ["pepperoni","basil","green peppers"];
+
+
+// Tests
+
+let myPizza = new Pizza(smallSize, toppings);
+console.log(myPizza);
+myPizza.cost();
 
 
