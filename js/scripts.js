49e331f7e75e4167958c916427eb282f5c7873d4
David McCaslin <davidmccaslin94@gmail.com>
2020-09-14 00:31:13 -0700

    modify scripts file to rearrange code to not operate at global level

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ function Pizza(name,Size,toppings) {
 }
 
 Pizza.prototype.cost = function(sizeCost) {
-  //const baseCost = this.Size.baseCost;
   const baseCost = sizeCost;
   const toppingCost = this.toppings.length * 2;
   this.cost = baseCost + toppingCost;
@@ -19,45 +18,34 @@ function Size(name,baseCost) {
   this.baseCost = baseCost
 }
 
-const smallSize = new Size("small",10);
-const mediumSize = new Size("medium",15);
-const largeSize = new Size("large",20);
+function initializeSizes() {
+  const smallSize = new Size("small",10);
+  const mediumSize = new Size("medium",15);
+  const largeSize = new Size("large",20);
 
-let sizes = [];
-sizes.push(smallSize);
-sizes.push(mediumSize);
-sizes.push(largeSize);
-
-// Business Logic for ~ Topping ~
-
-let toppings = ["pepperoni","basil","green peppers"];
-
-/*
-function Topping(name,cost) {
-  this.name = name,
-  this.cost = cost
+  let sizes = [];
+  sizes.push(smallSize);
+  sizes.push(mediumSize);
+  sizes.push(largeSize);
+  return sizes;
 }
-*/
-
 
 // UI Logic
 
 $(document).ready(function() {
+  const sizes = initializeSizes();
   const selectInput = $("select#pizzaSize");
   sizes.forEach(function(size) {
     selectInput.append("<option value=" + size.baseCost + ">" + size.name + "</option>");
   });
 
-
   $("form#pizzaSelection").submit(function() {
     event.preventDefault();
     const pizzaName = $("input#pizzaName").val();
-    const pizzaSize = parseInt($("select#pizzaSize").val());\
-    console.log(pizzaSize);
+    const pizzaSize = parseInt($("select#pizzaSize").val());
     let pizzaToppings = [];
     pizzaToppings.push($("input:radio[name=topping]:checked").val());
     const myPizza = new Pizza(pizzaName,pizzaSize,pizzaToppings);
-    console.log(myPizza);
     $("#pizzaCost").text(myPizza.cost(pizzaSize));
     $("#name").text(myPizza.name);
     $("#size").text(myPizza.Size.name);
@@ -66,17 +54,3 @@ $(document).ready(function() {
   })
 
 });
-
-
-/* To Do
-
-- update topping BL
-- create your own topping?
-
-*/
-
-/* Questions
-
-- parameter/variable names for objects - capitalized?
-
-*/
